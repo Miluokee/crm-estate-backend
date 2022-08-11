975f6ff6713319e3d13b2c0b5fc0192889ee4f3b
Miluokee <tim.srbk@gmail.com>
2022-08-11 10:16:11 +0300

    DB connected

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 import express from "express"
 import jsonwebtoken from "jsonwebtoken"
+import mongoose from "mongoose";
+
+mongoose
+    .connect('mongodb+srv://miluokee:1864f45T@mnt-estate-db.szaxcaj.mongodb.net/?retryWrites=true&w=majority')
+    .then(() => console.log('Database started on Mongoose'))
+    .catch((error) => console.log('Database error', error))
 
 const app = express()
 
@@ -9,7 +15,7 @@ app.get('/', (req, res) => {
     res.send('Server started with nodemon')
 })
 
-const JWT_TOKEN_KEY = 'dd8c9a8fe91c1c9617a7ec78151b05ff'
+const JWT_KEY = 'dd8c9a8fe91c1c9617a7ec78151b05ff'
 
 app.post('/auth/login', (req, res) => {
     console.log(req.body)
@@ -17,7 +23,7 @@ app.post('/auth/login', (req, res) => {
     const token = jsonwebtoken.sign({
         login: req.body.login,
         password: req.body.password
-    }, JWT_TOKEN_KEY)
+    }, JWT_KEY)
 
     res.json({
         success: true,
