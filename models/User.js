5ac5018e123264ec60dd5c7eb538b1322ffc3144
Miluokee <tim.srbk@gmail.com>
2022-08-11 16:33:02 +0300

    created user model in mongoDB

--- models/User.js
+++ models/User.js
@@ -4,5 +4,25 @@ const UserSchema = new mongoose.Schema({
     fullName: {
         type: String,
         required: true
-    }
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    passwordHash: {
+        type: String,
+        required: true
+    },
+    phoneNumber: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    avatarUrl: String,
+},
+{
+    timestamps: true
 })
+
+export default mongoose.model('User', UserSchema)
