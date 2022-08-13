bd1501001680663f96e19804605a1423e21882b1
Miluokee <tim.srbk@gmail.com>
2022-08-13 09:09:23 +0300

    add registration/login

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 import express from "express"
 import jsonwebtoken from "jsonwebtoken"
-import mongoose from "mongoose";
-import { validationResult } from "express-validator";
+import bcrypt from "bcrypt";
+import mongoose from "mongoose"
+import { validationResult } from "express-validator"
 
-import { registerValidation } from "./validations/auth.validation.js";
+import { registerValidation, loginValidation } from "./validations/auth.validation.js"
+
+import UserModel from "./models/user.model.js";
 
 mongoose
-    .connect('mongodb+srv://miluokee:1864f45T@mnt-estate-db.szaxcaj.mongodb.net/?retryWrites=true&w=majority')
+    .connect('mongodb+srv://miluokee:1864f45T@mnt-estate-db.szaxcaj.mongodb.net/mnt-estate?retryWrites=true&w=majority')
     .then(() => console.log('Database started on Mongoose'))
     .catch((error) => console.log('Database error', error))
 
@@ -20,15 +23,91 @@ server.get('/', (req, res) => {
 
 const JWT_KEY = 'dd8c9a8fe91c1c9617a7ec78151b05ff'
 
-server.post('/auth/register', registerValidation, (req, res) => {
-    const errors = validationResult(req)
-    if(!errors.isEmpty()) {
-        return res.status(400).json(errors.array())
+server.post('/auth/register', registerValidation, async (req, res) => {
+    try {
+        const errors = validationResult(req)
+        if(!errors.isEmpty()) {
+            return res.status(400).json(errors.array())
+        }
+    
+        const password = req.body.password
+        const salt = await bcrypt.genSalt(10)
+        const hash = await bcrypt.hash(password, salt)
+
+        const doc = new UserModel({
+            email: req.body.email,
+            fullName: req.body.fullName,
+            passwordHash: hash,
+            phoneNumber: req.body.phoneNumber,
+            avatarUrl: req.body.avatarUrl
+        })
+    
+        const user = await doc.save()
+
+        const token = jsonwebtoken.sign(
+            {
+                _id: user._id
+            },
+            JWT_KEY,
+            {
+                expiresIn: '30d'
+            }
+        )
+
+        const { passwordHash, ...userData } = user._doc
+    
+        res.json({
+            ...userData,
+            token
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            message: 'Не вдалось зареєструватись'
+        })
     }
-    res.json({
-        success: true
-    })
 })
+
+server.post('/auth/login', async (req, res) => {
+    try {
+        const user = await UserModel.findOne({ email: req.body.email })
+
+        if (!user) {
+            return req.status(404).json({
+                message: 'Користувач відсутній'
+            })
+        }
+
+        const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash)
+
+        if (!isValidPass) {
+            return res.status(400).json({
+                message: 'Неравильний логін або пароль'
+            })
+        }
+
+        const token = jsonwebtoken.sign({
+            _id: user._id
+        },
+        JWT_KEY,
+        {
+            expiresIn: '30d'
+        })
+
+        const { passwordHash, ...userData } = user._doc
+    
+        res.json({
+            ...userData,
+            token
+        })
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            message: 'Не вдалось авторизуватись'
+        })
+    }
+})
+
 
 server.listen(1864, (err) => {
     if (err) {
--- validations/auth.validation.js
+++ validations/auth.validation.js
@@ -1,16 +1,16 @@
 import { body } from "express-validator";
-import * as ValidationHelper from "../helpers/validation.helper.js";
+import * as ValidationHelper from "../helpers/auth.validation.helper.js";
 
 export const registerValidation = [
-    body('fullName').isString(),
+    body('fullName').isLength({ min: 3 }).isString(),
     body('email').isEmail(),
     body('confirmEmail').custom(ValidationHelper.isEmailSame),
-    body('password').isString(),
+    body('password').isString().isLength({ min: 8 }),
     body('confirmPassword').custom(ValidationHelper.isPasswordSame),  
     body('phoneNumber').custom(ValidationHelper.isPhoneNumber)
 ]
 
 export const loginValidation = [
     body('email').isEmail(),
-    body('password').isString()
+    body('password').isLength({ min: 8 }).isString()
 ]
